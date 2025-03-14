0b2294bb1256bb7e452e4a762df186969a830336
Noemi Collantes <noemicollantereyes24@gmail.com>
2025-03-14 11:48:34 -0500

    Rute configuration and new components

---
+++ src/app/page/seminars/seminars.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seminars',
+  imports: [],
+  templateUrl: './seminars.component.html',
+  styleUrl: './seminars.component.css'
+})
+export class SeminarsComponent {
+
+}
